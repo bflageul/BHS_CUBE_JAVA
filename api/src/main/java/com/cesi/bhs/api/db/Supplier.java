2180f8fd7620e13ebd4934cb2a6964f846a9588f
hugo <hugo.dumas0@gmail.com>
2022-02-08 14:07:05 +0100

    Dera lead dev i have fix my very good commentary

--- api/src/main/java/com/cesi/bhs/api/db/Supplier.java
+++ api/src/main/java/com/cesi/bhs/api/db/Supplier.java
@@ -1,19 +1,21 @@
 
 package com.cesi.bhs.api.db;
 
-/** Supplier interface define , Supplier class with his variable , it contains address (Class Address)
+/** Supplier interface define ,
+ * Supplier class with his variable ,
+ * it contains address (Class Address)
  * name, phone, id **/
 
 public interface Supplier {
 
-  /** getter de supplier **/
+
 
   public int getId();
   public Address getAddress();
   public String getName();
   public String getPhone();
 
-  /** setters de Supllier **/
+
 
   public void setAddress(Address address);
   public void setId(int id);
--- api/src/main/java/com/cesi/bhs/api/db/SupplierImpl.java
+++ api/src/main/java/com/cesi/bhs/api/db/SupplierImpl.java
@@ -1,5 +1,7 @@
 package com.cesi.bhs.api.db;
-/** classe de modele Supplier avec les meme champs de nommage que la db supplier -> fournisseurs  **/
+
+/** Implementation for Supplier ,
+ *  and hello Lead Tech**/
 
 public class SupplierImpl {
   private int id;
@@ -7,7 +9,7 @@ public class SupplierImpl {
   private Address address;
   private String phone;
 
-  /** constructor de Supplier **/
+
 
   public SupplierImpl(int id, String name, Address address, String phone) {
     this.id = id;
@@ -16,36 +18,31 @@ public class SupplierImpl {
     this.phone = phone;
   }
 
-  /** getter de supplier **/
+
 
   public int getId() {
     return id;
   }
-
   public Address getAddress() {
     return address;
   }
-
   public String getName() {
     return name;
   }
   public String getPhone() {
     return phone;
   }
-  /** setters de Supllier **/
+
+
   public void setAddress(Address address) {
     this.address = address;
   }
-
-
   public void setId(int id) {
     this.id = id;
   }
-
   public void setName(String name) {
     this.name = name;
   }
-
   public void setPhone(String phone) {
     this.phone = phone;
   }
