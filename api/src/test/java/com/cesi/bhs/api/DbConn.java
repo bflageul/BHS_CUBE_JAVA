a7f2c967fe816250624570d7a93b601931ff7bd0
hugo <hugo.dumas0@gmail.com>
2022-02-08 10:03:26 +0100

    db conn add

---
+++ api/src/test/java/com/cesi/bhs/api/DbConn.java
@@ -0,0 +1,26 @@
+package com.cesi.bhs.api;
+
+public class DbConn {
+  private final String url = "jdbc:postgresql://localhost/stivedb";
+  private final String user = "cesi";
+  private final String password = "cesi";
+
+  public static void printSQLException(SQLException ex) {
+    for (Throwable e : ex) {
+      if (e instanceof SQLException) {
+        e.printStackTrace(System.err);
+        System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+        System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+        System.err.println("Message: " + e.getMessage());
+        Throwable t = ex.getCause();
+        while (t != null) {
+          System.out.println("Cause: " + t);
+          t = t.getCause();
+        }
+      }
+    }
+  }
+}
+
+
+
