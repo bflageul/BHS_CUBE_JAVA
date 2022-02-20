6e44c0e72077e536c294dcd4684763e5caed218c
Eragon <eragon@eragon.re>
2022-02-20 12:13:48 +0100

    Fix token validation and add usage example

--- api/src/main/java/com/cesi/bhs/api/AuthenticationHandler.java
+++ api/src/main/java/com/cesi/bhs/api/AuthenticationHandler.java
@@ -1,15 +1,16 @@
 package com.cesi.bhs.api;
 
-import com.cesi.bhs.api.authentication.AuthenticationManager;
-import com.cesi.bhs.api.authentication.AuthenticationManagerImpl;
-import com.cesi.bhs.api.authentication.LoginUser;
-import com.cesi.bhs.api.authentication.RegistrationUser;
-import com.cesi.bhs.api.data.*;
+import com.cesi.bhs.api.authentication.*;
+import com.cesi.bhs.api.data.Client;
+import com.cesi.bhs.api.data.ClientImpl;
+import com.cesi.bhs.api.data.SimpleHttpResult;
+import com.cesi.bhs.api.data.Users;
 import io.vertx.core.json.DecodeException;
 import io.vertx.core.json.Json;
 import io.vertx.ext.web.RoutingContext;
+import org.jetbrains.annotations.NotNull;
 
-import java.sql.*;
+import java.sql.SQLException;
 import java.util.regex.Pattern;
 
 import static com.cesi.bhs.api.dao.Authentication.getUser;
@@ -34,7 +35,7 @@ public class AuthenticationHandler {
         return;
       }
 
-      AuthenticationManager authenticationManager = new AuthenticationManagerImpl();
+      AuthenticationManager authenticationManager = AuthenticationManagerImpl.getInstance();
 
       String token = authenticationManager.login(user, loginUser.password);
 
@@ -54,28 +55,28 @@ public class AuthenticationHandler {
     } catch (SQLException e) {
       e.printStackTrace();
       routingContext.response()
-       .setStatusCode(500)
-       .putHeader("content-type", "application/json; charset=utf-8")
-       .end(Json.encodePrettily(new SimpleHttpResult(500, "Internal server error")));
+        .setStatusCode(500)
+        .putHeader("content-type", "application/json; charset=utf-8")
+        .end(Json.encodePrettily(new SimpleHttpResult(500, "Internal server error")));
     } catch (DecodeException e) {
       e.printStackTrace();
       routingContext.response()
-       .setStatusCode(400)
-       .putHeader("content-type", "application/json; charset=utf-8")
-       .end(Json.encodePrettily(new SimpleHttpResult(400, "Invalid JSON")));
+        .setStatusCode(400)
+        .putHeader("content-type", "application/json; charset=utf-8")
+        .end(Json.encodePrettily(new SimpleHttpResult(400, "Invalid JSON")));
     } catch (NullPointerException e) {
       e.printStackTrace();
       routingContext.response()
-       .setStatusCode(400)
-       .putHeader("content-type", "application/json; charset=utf-8")
-       .end(Json.encodePrettily(new SimpleHttpResult(400, "Body is empty")));
+        .setStatusCode(400)
+        .putHeader("content-type", "application/json; charset=utf-8")
+        .end(Json.encodePrettily(new SimpleHttpResult(400, "Body is empty")));
     }
   }
 
   /**
    * Check the user data, and if unique register the user in database
    */
-  public static void register(RoutingContext routingContext) {
+  public static void register(@NotNull RoutingContext routingContext) {
     // Get data from request
     try {
       final RegistrationUser registrationUser = Json.decodeValue(routingContext.getBodyAsString(), RegistrationUser.class);
@@ -122,7 +123,7 @@ public class AuthenticationHandler {
 
       Client client = new ClientImpl(registrationUser);
 
-      AuthenticationManager authenticationManager = new AuthenticationManagerImpl();
+      AuthenticationManager authenticationManager = AuthenticationManagerImpl.getInstance();
 
       String registrationResult = authenticationManager.register(client);
 
@@ -134,9 +135,9 @@ public class AuthenticationHandler {
         return;
       } else if (registrationResult == "Registration failed, internal server error") {
         routingContext.response()
-        .setStatusCode(500)
-        .putHeader("content-type", "application/json; charset=utf-8")
-        .end(Json.encodePrettily(new SimpleHttpResult(500, registrationResult)));
+          .setStatusCode(500)
+          .putHeader("content-type", "application/json; charset=utf-8")
+          .end(Json.encodePrettily(new SimpleHttpResult(500, registrationResult)));
         return;
       }
 
@@ -148,10 +149,28 @@ public class AuthenticationHandler {
     } catch (DecodeException e) {
       e.printStackTrace();
       routingContext.response()
-       .setStatusCode(400)
-       .putHeader("content-type", "application/json; charset=utf-8")
-       .end(Json.encodePrettily(new SimpleHttpResult(400, "Invalid JSON")));
+        .setStatusCode(400)
+        .putHeader("content-type", "application/json; charset=utf-8")
+        .end(Json.encodePrettily(new SimpleHttpResult(400, "Invalid JSON")));
     }
+  }
+
+  /**
+   * Check if the user request is allowed in this context
+   */
+  public static void checkToken(@NotNull RoutingContext routingContext) {
+    final String authorizationHeader = routingContext.request().getHeader("Authorization");
+    final String token = authorizationHeader.split(" ")[1];
+
+    AuthenticationManager authenticationManager = AuthenticationManagerImpl.getInstance();
+
+    if (!authenticationManager.checkToken(token, AuthenticationContext.LOGIN)) {
+      routingContext.response()
+        .setStatusCode(401)
+        .putHeader("content-type", "application/json; charset=utf-8")
+        .end(Json.encodePrettily(new SimpleHttpResult(401, "You should login before accessing this resource")));
+    }
+    routingContext.next();
   }
 
   private static boolean emailValidation(String email) {
--- api/src/main/java/com/cesi/bhs/api/authentication/AuthenticationManagerImpl.java
+++ api/src/main/java/com/cesi/bhs/api/authentication/AuthenticationManagerImpl.java
@@ -3,8 +3,9 @@ package com.cesi.bhs.api.authentication;
 import com.cesi.bhs.api.data.Client;
 import com.cesi.bhs.api.data.Users;
 import org.javatuples.Pair;
+import org.jetbrains.annotations.NotNull;
 
-import java.sql.*;
+import java.sql.SQLException;
 import java.time.Instant;
 import java.util.Base64;
 import java.util.Calendar;
@@ -17,7 +18,20 @@ import static com.cesi.bhs.api.dao.Authentication.registerClient;
 public class AuthenticationManagerImpl implements AuthenticationManager {
   // A map with in A. the token, and in B. a pair of User and the creation date of the token
   static private final HashMap<String, Pair<Users, Date>> tokenMap = new HashMap<>();
+  // Service instance
+  private static AuthenticationManager instance;
 
+  private AuthenticationManagerImpl() {
+  }
+
+  public static AuthenticationManager getInstance() {
+    if (AuthenticationManagerImpl.instance == null) {
+      instance = new AuthenticationManagerImpl();
+    }
+    return instance;
+  }
+
+  @Override
   public boolean checkToken(String token, AuthenticationContext context) {
     Pair<Users, Date> dataPair = tokenMap.get(token);
 
@@ -45,30 +59,16 @@ public class AuthenticationManagerImpl implements AuthenticationManager {
     }
   }
 
-  public String login(Users user, String password) {
+  @Override
+  public String login(@NotNull Users user, String password) {
     if (user.validatePassword(password)) {
       return createToken(user);
     }
     return "Token creation failed";
   }
 
-  private String createToken(Users user) {
-    // Get current date
-    Date now = Date.from(Instant.now());
-
-    // Prepare the token string
-    StringBuilder tokenBuilder = new StringBuilder();
-    tokenBuilder.append(user.getId()).append(":").append(user.getUsername()).append(":").append(now.getTime());
-
-    // Encode the token
-    String token = Base64.getEncoder().encodeToString(tokenBuilder.toString().getBytes());
-
-    tokenMap.put(token, new Pair<>(user, now));
-
-    return token;
-  }
-
-  public String register(Client client) {
+  @Override
+  public String register(@NotNull Client client) {
     try {
       // Check if the username is available
       if (!isUsernameAvailable(client.getUsername())) {
@@ -82,5 +82,21 @@ public class AuthenticationManagerImpl implements AuthenticationManager {
     }
 
     return "Registration done, please login";
+  }
+
+  private String createToken(@NotNull Users user) {
+    // Get current date
+    Date now = Date.from(Instant.now());
+
+    // Prepare the token string
+    StringBuilder tokenBuilder = new StringBuilder();
+    tokenBuilder.append(user.getId()).append(":").append(user.getUsername()).append(":").append(now.getTime());
+
+    // Encode the token
+    String token = Base64.getEncoder().encodeToString(tokenBuilder.toString().getBytes());
+
+    tokenMap.put(token, new Pair<>(user, now));
+
+    return token;
   }
 }
