85c0a570b44129030e1962daebed21ec86731615
Eragon <eragon@eragon.re>
2022-02-28 22:13:23 +0100

    Fix product creation

--- api/src/main/java/com/cesi/bhs/api/dao/Product.java
+++ api/src/main/java/com/cesi/bhs/api/dao/Product.java
@@ -1,6 +1,5 @@
 package com.cesi.bhs.api.dao;
 
-import com.cesi.bhs.api.data.Client;
 import org.jetbrains.annotations.NotNull;
 
 import java.sql.*;
@@ -15,14 +14,14 @@ public class Product {
    */
   public static @NotNull com.cesi.bhs.api.data.Product registerProduct(@NotNull com.cesi.bhs.api.data.Product product) throws SQLException {
     Connect connect = ConnectImpl.getInstance();
-    PreparedStatement insertProduct = connect.getConnection().prepareStatement("Insert into Product (name, stock, description, type, origin, medal,birthdate, productorname) VALUES (?,?,?,?,?,?,?) RETURNING id");
+    PreparedStatement insertProduct = connect.getConnection().prepareStatement("Insert into Product (name, stock, description, producttype, origin, medal, birthdate, productorname) VALUES (?, ?, ?, ?, ?, ?, ?, ?) RETURNING id");
     insertProduct.setString(1, product.getName());
     insertProduct.setInt(2, product.getStock());
     insertProduct.setString(3, product.getDescription());
     insertProduct.setString(4, product.getType());
     insertProduct.setString(5, product.getOrigin());
     insertProduct.setString(6, product.getMedal());
-    insertProduct.setDate(7, (Date) product.getBirthdate());
+    insertProduct.setDate(7, new Date(product.getBirthdate().getTime()));
     insertProduct.setString(8, product.getProductorname());
     ResultSet resultSet = insertProduct.executeQuery();
     resultSet.next();
