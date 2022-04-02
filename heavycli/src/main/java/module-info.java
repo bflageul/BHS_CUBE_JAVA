b13ce146c69900c51982d0fbc8d2888d436f6fe2
Eragon <eragon@eragon.re>
2022-04-02 23:12:34 +0200

    Clean project and fix build issues

--- heavycli/src/main/java/module-info.java
+++ heavycli/src/main/java/module-info.java
@@ -1,15 +1,7 @@
-module com.negosud.heavycli {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
+module com.cesi.bhs.heavycli {
+  requires javafx.controls;
+  requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-
-    opens com.cesi.bhs.heavycli to javafx.fxml;
-    exports com.cesi.bhs.heavycli;
+  opens com.cesi.bhs.heavycli to javafx.fxml;
+  exports com.cesi.bhs.heavycli;
 }
